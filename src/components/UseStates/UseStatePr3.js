66218b46cdb0a9247a4657870d93c52dd06a9522
C112156103 <C112156103@nkust.edu.tw>
2025-03-27 09:44:28 +0800

    密碼遮蔽

--- src/components/UseStates/UseStatePr3.js
+++ src/components/UseStates/UseStatePr3.js
@@ -6,16 +6,25 @@ export default function UseStatePr3() {
     const [stuEmail, setEmail] = useState("nothing"); 
     const [stuVerify, setverify] = useState("nothing"); 
     
+    // 密碼遮蔽函式
+    const maskPassword = (password) => {
+        if(password === "nothing" || password === ""){
+            return "";
+        }
+        // 顯示第一個字元，其餘用星號替代
+        return password[0] + '*'.repeat(password.length - 1);
+    }
+    
     return (
         <>
-            <h2>useState練習3：帳號資訊</h2>
+            <h2>useState練習3&0327練習1：帳號資訊(密碼遮蔽)</h2>
             <input type="text" placeholder="請輸入帳號" onChange={(e)=>setAcc(e.target.value)}/><br/>
             <input type="password" placeholder="請輸入密碼" onChange={(e)=>setPwd(e.target.value)}/><br/>
             <input type="text" placeholder="請輸入電子郵件" onChange={(e)=>setEmail(e.target.value)}/><br/>
             <input type="text" placeholder="請輸入驗證碼" onChange={(e)=>setverify(e.target.value)}/>
             <br/>
             <p>帳號: {stuAcc}</p>
-            <p>密碼: {stuPwd}</p>
+            <p>密碼: {maskPassword(stuPwd)}</p>
             <p>電子郵件: {stuEmail}</p>
             <p>驗證碼: {stuVerify}</p>
         </>
